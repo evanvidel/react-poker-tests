9973513a3d720db6b9313cd029eb907b5fcf5e00
evanvidel <evanvidel@gmail.com>
2023-09-21 15:38:58 -0300

    component di mount image loaded

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,29 @@ import './Poker.css';
 
 import Spinner from './Spinner';
 import WinScreen from './WinScreen';
+/* descobrir como passar para outra pagina */
 
 class App extends Component {
   state = {
     loading: true,
     winnerFound: null,
   };
+
+  async componentDidMount() {
+    const imageLoaderRequest = new XMLHttpRequest();
+
+    imageLoaderRequest.addEventListener('load', (e) => {
+      console.log(`${e.type}`);
+      console.log(e);
+      console.log('Image Loaded!');
+      this.setState({ loading: false });
+    });
+
+    imageLoaderRequest.open('GET', './assets/table-nobg-svg-01.svg');
+    imageLoaderRequest.send();
+  }
+
+  renderGame = () => {};
   render() {
     return (
       <div className="App">
